3702b338a6b7a646dbaec7ebd85f6be5a7b3e8d4
Daniel Machado <danielmcis@gmail.com>
2022-09-14 20:49:25 +0000

    user endpoints modified

--- api/users.py
+++ api/users.py
@@ -1,10 +1,15 @@
 # pylint: disable=no-member
 
+"""User endpoints definition
+
+"""
+
+from datetime import datetime, timezone
 import json
 from flask import Blueprint, current_app, request
 from bson import json_util
 from api import db
-from api.models import User
+from api.models import UserRequest, UserResponse, validate_model
 
 users = Blueprint('users', __name__)
 
@@ -13,19 +18,37 @@ def objectid(item):
 
 @users.route('/users', methods=['GET'])
 def get_user():
+    """Get user list
+
+    Returns:
+        json: response
+        int: http status code
+    """
     current_app.logger.info("Quering all users")
-    result = list(db.cx.fidulancer.user.find({}))
-    list(map(objectid, result))
-    current_app.logger.info(result)
-    return json.loads(json_util.dumps(result)), 200
+    results = list(db.cx.fidulancer.user.find({}))
+    errors = []
+    for result in results:
+        validate_model(UserResponse, result)
+    current_app.logger.info(results)
+    return json.loads(json_util.dumps(results)), 200
 
 
 @users.route('/users', methods=['POST'])
-@validate(body=User)
 def new_user():
+    """Creates new user
+
+    Returns:
+        json: response
+        int: http status code
+    """
     current_app.logger.info("Creating new item")
     body = request.get_json()
-    user =  User(**body)
-    current_app.logger.info(user)
-    result = db.cx.fidulancer.user.insert_one(user)
-    return json.loads(json_util.dumps(result.inserted_id)), 201
+
+    validate_model(UserRequest, body)
+
+    body["created_at"] = datetime.utcnow().replace(tzinfo=timezone.utc)
+    body["updated_at"] = datetime.min.replace(tzinfo=timezone.utc)
+    
+    current_app.logger.info(body)
+    save_result = db.cx.fidulancer.user.insert_one(body)
+    return json.loads(json_util.dumps(save_result.inserted_id)), 201
