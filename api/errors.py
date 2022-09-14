3397362c66f3904e8a4181bee41c34dc83dd0d0b
Daniel Machado <danielmcis@gmail.com>
2022-09-14 20:45:01 +0000

    new validation error

--- api/errors.py
+++ api/errors.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
+from typing import Dict
 from flask import Blueprint, current_app
 from werkzeug.exceptions import HTTPException, InternalServerError
-from mongoengine import ValidationError
+from pydantic import ValidationError
 
 errors = Blueprint('errors', __name__)
 
@@ -13,11 +14,23 @@ def http_error(error):
         'description': error.description,
     }, error.code
 
+
 @errors.app_errorhandler(ValidationError)
-def validation_error(error):
-    fields = [field for field in error.errors]
+def validation_error(result):
+    return [extract_error_data(error) for error in result.errors()] , HTTPStatus.BAD_REQUEST
+
+
+
+def extract_error_data(error: Dict) -> Dict:
+    """select dict keys to be included in response
+
+    Args:
+        error (Dict): data about validation error found
+
+    Returns:
+        Dict: selected keys
+    """
     return {
-        'code': HTTPStatus.BAD_REQUEST,
-        'message': error.message,
-        'fields': fields,
-    }, HTTPStatus.BAD_REQUEST
+        "field": error["loc"][0],
+        "message": error["msg"]
+    }
