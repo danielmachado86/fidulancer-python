ebd4f5d8dd5061f359bd20e06b7f23511b08c397
Daniel Machado <danielmcis@gmail.com>
2022-09-15 18:33:24 +0000

    added shell context processor

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 
-from flask import Flask
+from flask import Flask, request
 from flask_pymongo import PyMongo
+from pydantic import BaseModel
 
 from config import Config
 
@@ -12,6 +13,7 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
+    from api import models
     db.init_app(app)
 
     from api.errors import errors
@@ -19,4 +21,21 @@ def create_app(config_class=Config):
     from api.users import users
     app.register_blueprint(users, url_prefix='/v1')
 
+    # define the shell context
+    @app.shell_context_processor
+    def shell_context():  # pragma: no cover
+        ctx = {'db': db.cx.fidulancer}
+        for attr in dir(models):
+            model = getattr(models, attr)
+            if hasattr(model, '__bases__') and \
+                    BaseModel in getattr(model, '__bases__'):
+                ctx[attr] = model
+        app.logger.info(ctx)
+        return ctx
+
+    @app.after_request
+    def after_request(response):
+        # Werkzeug sometimes does not flush the request body so we do it here
+        request.get_data()
+        return response
     return app
