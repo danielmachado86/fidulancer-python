fc6c1b4b39f4a803b004d1175e486add379a280d
Daniel Machado <danielmcis@gmail.com>
2022-11-01 08:25:04 -0500

    config definition

--- config.py
+++ config.py
@@ -3,10 +3,12 @@
 """
 
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 def as_bool(value):
     """_summary_
@@ -18,24 +20,30 @@ def as_bool(value):
         _type_: _description_
     """
     if value:
-        return value.lower() in ['true', 'yes', 'on', '1']
+        return value.lower() in ["true", "yes", "on", "1"]
     return False
 
+
 class Config:
-    """Config class
-    """
+    """Config class"""
+
     # security options
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'top-secret!')
-    DISABLE_AUTH = as_bool(os.environ.get('DISABLE_AUTH'))
-    ACCESS_TOKEN_MINUTES = int(os.environ.get('ACCESS_TOKEN_MINUTES') or '15')
-    REFRESH_TOKEN_DAYS = int(os.environ.get('REFRESH_TOKEN_DAYS') or '7')
-    REFRESH_TOKEN_IN_COOKIE = as_bool(os.environ.get(
-        'REFRESH_TOKEN_IN_COOKIE') or 'yes')
-    REFRESH_TOKEN_IN_BODY = as_bool(os.environ.get('REFRESH_TOKEN_IN_BODY'))
-    RESET_TOKEN_MINUTES = int(os.environ.get('RESET_TOKEN_MINUTES') or '15')
-    PASSWORD_RESET_URL = os.environ.get('PASSWORD_RESET_URL') or \
-        'http://localhost:3000/reset'
-    USE_CORS = as_bool(os.environ.get('USE_CORS') or 'yes')
-    CORS_SUPPORTS_CREDENTIALS = True,
-    MONGO_URI = os.environ.get('MONGO_URI') or \
-        'mongodb://root:example@mongodb:27017/'
+    SECRET_KEY = os.environ.get("SECRET_KEY", "top-secret!")
+    DISABLE_AUTH = as_bool(os.environ.get("DISABLE_AUTH"))
+    ACCESS_TOKEN_MINUTES = int(os.environ.get("ACCESS_TOKEN_MINUTES") or "15")
+    REFRESH_TOKEN_DAYS = int(os.environ.get("REFRESH_TOKEN_DAYS") or "30")
+    REFRESH_TOKEN_IN_COOKIE = as_bool(
+        os.environ.get("REFRESH_TOKEN_IN_COOKIE") or "yes"
+    )
+    REFRESH_TOKEN_IN_BODY = as_bool(os.environ.get("REFRESH_TOKEN_IN_BODY"))
+    RESET_TOKEN_MINUTES = int(os.environ.get("RESET_TOKEN_MINUTES") or "15")
+    PASSWORD_RESET_URL = (
+        os.environ.get("PASSWORD_RESET_URL") or "http://localhost:3000/reset"
+    )
+    USE_CORS = as_bool(os.environ.get("USE_CORS") or "yes")
+    CORS_SUPPORTS_CREDENTIALS = (True,)
+    MONGO_URL = os.environ.get("MONGO_URL") or "mongodb"
+    MONGO_PORT = int(os.environ.get("MONGO_PORT") or "27017")
+    MONGO_USER = os.environ.get("MONGO_USER") or "root"
+    MONGO_PASSWORD = os.environ.get("MONGO_PASSWORD") or "example"
+    MONGO_DATABASE = os.environ.get("MONGO_DATABASE") or "fidulancer"
